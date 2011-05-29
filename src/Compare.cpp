9834b0aca6190ae343d0f96ef1ace886321717c6
Jean-Sebastien Leroy <jean.sebastien.leroy@gmail.com>
2011-05-29 10:13:27 +0200

    Update Compare source code to GPLv3

--- src/Compare.cpp
+++ src/Compare.cpp
@@ -1,23 +1,20 @@
-//this file is part of notepad++
-//Copyright (C)2003 Don HO ( donho@altern.org )
-//
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/*
+This file is part of Compare plugin for Notepad++
+Copyright (C)2011 Jean-Sébastien Leroy (jean.sebastien.leroy@gmail.com)
 
-//--------------------------------------------------------------------------
-// Compare plugin by Ty Landercasper & Jean-sébastien Leroy
-//--------------------------------------------------------------------------
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "Compare.h"
 #include "NPPHelpers.h"
--- src/Compare.h
+++ src/Compare.h
@@ -1,11 +1,11 @@
 /*
-This file is part of Explorer Plugin for Notepad++
-Copyright (C)2006 Jens Lorenz <jens.plugin.npp@gmx.de>
+This file is part of Compare plugin for Notepad++
+Copyright (C)2011 Jean-Sébastien Leroy (jean.sebastien.leroy@gmail.com)
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,10 +13,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #ifndef COMPARE_H
 #define COMPARE_H
