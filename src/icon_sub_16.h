02f30e4c8dac881e9f11280ed5e27c38c93dc28b
Pavel Nedev <pg.nedev@gmail.com>
2016-03-11 01:18:31 +0200

    Change compare results icons (thanks to Yaron!)

--- src/icon_sub_16.h
+++ src/icon_sub_16.h
@@ -1,71 +1,38 @@
-/*
-This file is part of Compare plugin for Notepad++
-Copyright (C)2011 Jean-Sébastien Leroy (jean.sebastien.leroy@gmail.com)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 /* XPM */
 static const char * icon_sub_16_xpm[] = {
-"16 16 33 1",
-" 	c None",
-".	c #BC4C2E",
-"+	c #BF5137",
-"@	c #D14D3E",
-"#	c #CE513D",
-"$	c #C95643",
-"%	c #DB564B",
-"&	c #D25E50",
-"*	c #E95A4D",
-"=	c #DD6056",
-"-	c #EB5D55",
-";	c #D9635B",
-">	c #EA645D",
-",	c #EE6553",
-"'	c #DE6865",
-")	c #E36E64",
-"!	c #F36D5B",
-"~	c #F37863",
-"{	c #EC7B72",
-"]	c #E9807C",
-"^	c #F8846D",
-"/	c #EF8879",
-"(	c #F8917F",
-"_	c #EE958F",
-":	c #F99B89",
-"<	c #F4A09C",
-"[	c #F4AAA5",
-"}	c #FBAA9A",
-"|	c #F8ACA0",
-"1	c #F8B7AC",
-"2	c #FBC7BD",
-"3	c #FCD7D1",
-"4	c #FEFFFC",
-"                ",
-"      ''';      ",
-"    '_2321)&    ",
-"   '[31::}2{&   ",
-"  '[3::(((^1)$  ",
-"  ]3:(/^^^^~|@  ",
-" '[}/------~/{+ ",
-" '1(^444444*!<+ ",
-" ;|^^444444*>_. ",
-" ;/(!*******{]. ",
-"  %|,,*****-<.  ",
-"  &)|-****-<&.  ",
-"   $=[/>-]<=.   ",
-"    $#{__{..    ",
-"      ++..      ",
-"                "
+"14 14 20 1",
+"!	c None",
+"!	c #E4E4E4",
+"#	c #DF0000",
+"$	c #E90000",
+"%	c #DC0000",
+"&	c #FC4141",
+"'	c #FD6464",
+"(	c #FD6363",
+")	c #FC4B4B",
+"*	c #D00000",
+"+	c #FC3F3F",
+",	c #FC3131",
+"-	c #BB0000",
+".	c #FB1616",
+"0	c #D20000",
+"1	c #FB1C1C",
+"2	c #A00000",
+"3	c #E80000",
+"4	c #920000",
+"5	c #870000",
+"!!!!!!!!!!!!!!",
+"!!!!!!!!!!!!!!",
+"!!!!!!!!!!!!!!",
+"!!!!!!!!!!!!!!",
+"#$$$$$$$$$$$$#",
+"%&'((((((((()%",
+"%&'((((((((()%",
+"*+,,,,,,,,,,)*",
+"-.00000000001-",
+"23333333333332",
+"45555555555554",
+"!!!!!!!!!!!!!!",
+"!!!!!!!!!!!!!!",
+"!!!!!!!!!!!!!!"
 };
--- src/icon_warning_16.h
+++ src/icon_warning_16.h
@@ -1,71 +1,31 @@
-/*
-This file is part of Compare plugin for Notepad++
-Copyright (C)2011 Jean-Sébastien Leroy (jean.sebastien.leroy@gmail.com)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 /* XPM */
 static const char * icon_warning_16_xpm[] = {
-"16 16 33 1",
-" 	c None",
-".	c #C16700",
-"+	c #C77307",
-"@	c #D18713",
-"#	c #D8951F",
-"$	c #D29345",
-"%	c #DB9D26",
-"&	c #D7A139",
-"*	c #DFA62E",
-"=	c #E5AA29",
-"-	c #E6B12E",
-";	c #DDB245",
-">	c #E9BA35",
-",	c #E4BC6A",
-"'	c #E6C07B",
-")	c #E8BF87",
-"!	c #EAC367",
-"~	c #ECCB52",
-"{	c #EACEAC",
-"]	c #F1D185",
-"^	c #F5D74E",
-"/	c #F0D584",
-"(	c #F5D956",
-"_	c #F0D692",
-":	c #F6DC60",
-"<	c #F3D79E",
-"[	c #F7E2BD",
-"}	c #F8E893",
-"|	c #F8E7AD",
-"1	c #FCF3C4",
-"2	c #FAF3D4",
-"3	c #FEFAEA",
-"4	c #FDFCF5",
-"                ",
-"       >>       ",
-"      >/]       ",
-"      >42-      ",
-"     >_43_=     ",
-"     >4}}3*     ",
-"    >|3&&2<*    ",
-"    !4(&&(3,    ",
-"   -23:&&(1[%   ",
-"  -]3}(;&((2'@  ",
-"  -41((:~(^}3@  ",
-" -<3}((&&(^^2)+ ",
-" !4|((((((^^:3$ ",
-"-[433333333334{.",
-"=**%%%##@@@+++..",
-"                "
+"14 14 13 1",
+"!	c None",
+"!	c #E4E4E4",
+"#	c #E1191F",
+"$	c #C63137",
+"%	c #FF8D23",
+"&	c #FF932F",
+"'	c #A14344",
+"(	c #FF791A",
+")	c #FF7A1B",
+"*	c #AB5C30",
+"+	c #9F572C",
+",	c #784749",
+"-	c #3E191B",
+"!!!!!!##!!!!!!",
+"!!!!!!$$!!!!!!",
+"%&&&&&''&&&&&%",
+"(&))))''))))&(",
+"*+++++''+++++*",
+"!!!!!!''!!!!!!",
+"%&&&&&''&&&&&%",
+"(&)))),,))))&(",
+"*+++++,,+++++*",
+"!!!!!!,,!!!!!!",
+"!!!!!!--!!!!!!",
+"!!!!!!!!!!!!!!",
+"!!!!!!##!!!!!!",
+"!!!!!!--!!!!!!"
 };
