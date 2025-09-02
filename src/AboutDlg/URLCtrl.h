d92f5802a5fe0a5a0245d30e1a08fc7d16122b18
Pavel Nedev <pg.nedev@gmail.com>
2025-09-02 17:52:22 +0300

    Update URL Ctrl from Notepad++ codebase

--- src/AboutDlg/URLCtrl.h
+++ src/AboutDlg/URLCtrl.h
@@ -1,19 +1,18 @@
-//this file is part of notepad++
-//Copyright (C)2003 Don HO ( donho@altern.org )
+// This file is part of Notepad++ project
+// Copyright (C)2021 Don HO <don.h@free.fr>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option any later version.
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program.	 If not, see <https://www.gnu.org/licenses/>.
 
 
 #pragma once
@@ -24,32 +23,28 @@
 #include "DockingFeature/Window.h"
 
 
-class URLCtrl : public Window
-{
+class URLCtrl : public Window {
 public:
-	URLCtrl():_hfUnderlined(0),_hCursor(0), _oldproc(NULL),
-		_linkColor(), _visitedColor(), _clicking(false) {};
+	void create(HWND itemHandle, const wchar_t * link, COLORREF linkColor = RGB(0,0,255));
+	void create(HWND itemHandle, int cmd, HWND msgDest = NULL);
+	void destroy() override;
 
-	void create(HWND itemHandle, const wchar_t* link, COLORREF linkColor = RGB(0, 0, 255));
+private:
+	HCURSOR& loadHandCursor();
+	void action();
 
-	void destroy()
-	{
-			if(_hfUnderlined)
-				::DeleteObject(_hfUnderlined);
-			if(_hCursor)
-				::DestroyCursor(_hCursor);
-	};
-
-private :
+protected :
 	std::wstring	_URL;
-	HFONT			_hfUnderlined;
-	HCURSOR			_hCursor;
+	HFONT			_hfUnderlined	= nullptr;
+	HCURSOR			_hCursor		= nullptr;
 
-	WNDPROC		_oldproc;
-	COLORREF	_linkColor;
-	COLORREF	_visitedColor;
+	HWND			_msgDest	= nullptr;
+	unsigned long	_cmdID		= 0;
 
-	bool	_clicking;
+	WNDPROC		_oldproc		= nullptr;
+	COLORREF	_linkColor		= RGB(0xFF, 0xFF, 0xFF);
+	COLORREF	_visitedColor	= RGB(0xFF, 0xFF, 0xFF);
+	bool		_clicking		= false;
 
 	static LRESULT CALLBACK URLCtrlProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 	{
