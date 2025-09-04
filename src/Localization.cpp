df85df801bf36e7ea8ea033b4d51382fccb5f467
Pavel Nedev <pg.nedev@gmail.com>
2025-09-04 11:52:53 +0300

    Fix build problems

--- src/Localization.cpp
+++ src/Localization.cpp
@@ -1,10 +1,33 @@
+/*
+ * This file is part of ComparePlus plugin for Notepad++
+ * Author: Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Localization.h"
 #include "NppHelpers.h"
 #include <windows.h>
 #include <shlwapi.h>
 #include <fstream>
-#include <mutex>
 #include <unordered_map>
+
+#if defined(__MINGW32__) && !defined(_GLIBCXX_HAS_GTHREADS)
+#include "../mingw-std-threads/mingw.mutex.h"
+#else
+#include <mutex>
+#endif // __MINGW32__ ...
 
 // Minimal localization:
 // - Language INI files live in <plugin>/languages/<lang>.ini (fallback relative ./languages/ during development)
--- src/Localization.h
+++ src/Localization.h
@@ -1,5 +1,6 @@
 /*
  * This file is part of ComparePlus plugin for Notepad++
+ * Author: Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
